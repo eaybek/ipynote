f838f15e1d0320aa1bdcd957457b858958c36e83
Erdem Aybek <eaybek@gmail.com>
2019-09-07 13:36:02 +0300

    add a note taking object and module for ipython

--- setup.py
+++ setup.py
@@ -5,10 +5,12 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ipynote",
-    version="0.0.1",
+    version="0.0.2",
     author="Erdem Aybek",
     author_email="eaybek@gmail.com",
-    description=" ".join(["Nothing"]),
+    description=" ".join(
+        ["a little note taking object inside ipython"]
+    ),
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/eaybek/ipynote",
